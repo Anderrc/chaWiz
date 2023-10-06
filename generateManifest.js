229298fc55962c79df3459ccf7a09f210c481f3a
anderson.castano <anderson.castano@pragma.com.co>
2023-10-06 08:55:09 -0500

    se agrega el contextMenu

--- generateManifest.js
+++ generateManifest.js
@@ -11,12 +11,16 @@ const manifest = {
   "name": "ChaWiz",
   "version": "1.0",
   "description": "Con ChaWiz, optimiza tu flujo de trabajo y ahorra tiempo al realizar tareas relacionadas con el manejo de caracteres. Ya sea que seas un desarrollador, un escritor o un editor de contenido, ChaWiz es una herramienta versátil que te ayudará a mejorar la eficiencia y la calidad de tus textos.",
+  "permissions": ["contextMenus"],
   "icons": {
     "128": "./logo.png"
   },
   "action": {
     "default_popup": "./index.html"
   },
+  "background": {
+    "service_worker": "./contextMenu.js"
+  }
 };
 
 createTextFile(manifest, './dist/manifest.json');
